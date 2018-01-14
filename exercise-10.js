f48fd7e1b23d57cc94f6a8781635a66cde31361a
Reza Pramudhika <rezapramudhika@gmail.com>
2018-01-14 17:03:44 +0700

    Update exercise-10, exercise-13

--- exercise-10.js
+++ exercise-10.js
@@ -7,28 +7,24 @@ harus mencari angka selanjutnya yang palindrome. Contoh, jika angka adalah 8,
 walaupun dia sudah palindrome, harus mencari angka selanjutnya yang palindrome, yaitu 9. */
 
 function angkaPalindrome(num) {
-    var numToString = num.toString();
-    var reverseNum ='';
-    for(var i=numToString.length-1; i>=0; i--){
-        reverseNum += parseInt(numToString[i]);
+    var temp = num;
+    var numReverse;
+    var splitString;
+
+    while (temp != numReverse){
+        temp++;
+        temp = temp.toString();
+        splitString = temp.split('');
+        numReverse = splitString.reverse().join('');
     }
 
-    var hell = Number(reverseNum);
+    return Number.numReverse;
 
-    if (num != hell){
-        hell = false;
-    }else if (num==hell){
-        hell = hell+1;
-    }
-
-    return hell;
-  }
-  
+}
+ 
   // TEST CASES
   console.log(angkaPalindrome(8)); // 9
   console.log(angkaPalindrome(10)); // 11
   console.log(angkaPalindrome(117)); // 121
   console.log(angkaPalindrome(175)); // 181
   console.log(angkaPalindrome(1000)); // 1001
-
-  /**FAIL**/
--- exercise-13.js
+++ exercise-13.js
@@ -6,20 +6,48 @@ maka jarak terdekat dari 'o' ke 'x' adalah 2. Jika tidak ditemukan 'x' sama seka
 function akan me-return nilai 0. */
 
 function targetTerdekat(arr) {
-    var a = 'o';
-    var b = 'x';
-    var index_O = 0;
-    var index_X = 0;
+    var o = 'o';
+    var x = 'x';
+    var index_o = arr.indexOf('o');
+    var temp = 0;
+    var temp2 = 0;
+    var pertambahanKeKanan;
+    var pertambahanKeKiri;
+    var result;
 
-    for(i=0; i<=arr.length-1; i++){
-        if(arr[i]==a){
-            index_O = i;
-            console.log('dump A = '+index_O);
-        }else if(arr[i]==b){
-            index_X = i;
-            console.log('dump B = '+index_X);
+    for(i=index_o; i<=arr.length-1; i++){
+        if(arr[i]==x){
+            if (temp == 0){
+                temp = i;
+                pertambahanKeKanan = (index_o - temp);  
+            }
         }
     }
+    
+    for(i=index_o; i>=0; i--){     
+        if(arr[i]==x){
+            if (temp2 == 0){
+                temp2 = i;
+                pertambahanKeKiri = (index_o - temp2);  
+            }
+        }
+    }
+
+    if (pertambahanKeKiri < pertambahanKeKanan){
+        result = pertambahanKeKiri
+    }else{
+        result = pertambahanKeKanan
+    }
+    
+    if (pertambahanKeKanan === undefined){
+        result = pertambahanKeKiri
+    }
+
+    if (pertambahanKeKanan === undefined && pertambahanKeKiri === undefined){
+        result = 0;
+    }
+
+    return Math.abs(result);
   }
   
   // TEST CASES
@@ -28,25 +56,3 @@ function targetTerdekat(arr) {
   console.log(targetTerdekat(['x', '', '', '', 'x', 'x', 'o', ''])); // 1
   console.log(targetTerdekat(['', '', 'o', ''])); // 0
   console.log(targetTerdekat(['', 'o', '', 'x', 'x', '', '', 'x'])); // 2
-
-
-var numbers= ["2", "3", "5","7","1","2","3","4","8"];
-var x ="5";
-var y ="8";
-var firstIndex = numbers.indexOf(x);
-
-var minD = numbers.length;
-
-var x= numbers.forEach(function(item,index){
-    if((item == x) || (item == y)){
-    if((index != firstIndex) && (index-firstIndex < minD)){
-        minD = index-firstIndex;
-        firstIndex = index;
-    }else{
-        firstIndex = index;
-    }
- }
-});
-alert(minD);
-
- /**FAIL**/
